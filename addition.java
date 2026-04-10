5ee98cd2475f58fcf5290ddf4f687aa0962d89b8
Sirivalli22 <245123748303@mvsrec.edu.in>
2026-04-10 12:51:55 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,12 @@
+public class AdditionExample {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        
+        // Use the '+' operator to add values
+        int sum = number1 + number2;
+        
+        System.out.println("The sum is: " + sum);
+    }
+}
+
